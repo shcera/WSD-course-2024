27a88a42884dde58bb46b05be96d4f56966cf83e
shcera <shcannan30@gmail.com>
2024-10-29 20:48:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
+import * as storeService from "./store.js";
 
 const app = new Hono();
 
-app.get("/",
-    (c) => c.text(`Hi again!`));
-
-
+app.get("/", (c) => {
+    if (c.req.query("store")) {
+        storeService.setStore(c.req.query("store"));}
+    return c.text(`Store: ${storeService.getStore()}`)
+});
 
 export default app;
