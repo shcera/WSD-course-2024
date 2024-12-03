b453f871e95ec3d5572ee496e15c7b8099cb5eb8
shcera <shcannan30@gmail.com>
2024-12-03 18:14:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
+
+const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hi there!"));
+app.get("/", (c) => c.html(eta.render("index.eta")));
+app.post("/addresses", async (c) => {
+  const body = await c.req.parseBody();
+  const nimi = console.log(`Name: ${body.name}`);
+  const osoite = console.log(`Address: ${body.address}`)
+  return c.text(`${name} lives at ${osoite}.`);
+});
 
-export default app;
+Deno.serve(app.fetch);
