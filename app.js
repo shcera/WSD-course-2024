392e813eb6a934df75543e7532c9a2880f553c9b
shcera <shcannan30@gmail.com>
2025-02-11 20:50:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
-import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
-import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
-
-const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
+import { Hono } from "jsr:@hono/hono@4.6.5";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.html(eta.render("index.eta"));
-});
+app.get("/", (c) => c.text("Hi there!"));
 
-app.post("/", async (c) => {
-  const body = await c.req.parseBody(); // Parse form data
-  const name = body.name || "Unknown";  // Handle missing fields
-  const address = body.address || "Unknown";
-  
-  return c.text(`${name} lives at ${address}.`);
-});
-
-Deno.serve(app.fetch);
+export default app;
